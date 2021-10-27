868ccf9390af80cf06e7d3dac099bc10a7ff258b
Jerald <jeraldssw@gmail.com>
2021-10-27 12:24:52 +0800

    Add AdjacencySet component

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -5,6 +5,7 @@ import BoxCard from "../components/boxCard"
 import ProfileCard from "../components/profileCard"
 import { deleteAdjacencyList, generateAdjacencyList } from "src/actions/adjacencyList"
 import { deleteAdjacencyMatrix, generateAdjacencyMatrix } from "src/actions/adjacencyMatrix"
+import { deleteAdjacencySet, generateAdjacencySet } from "src/actions/adjacencySet"
 import { getStatus } from "src/actions/status"
 import { PuffLoader } from "react-spinners"
 import { addFriends, getRandomFriends } from "src/actions/friend"
@@ -13,10 +14,15 @@ import { addFriends, getRandomFriends } from "src/actions/friend"
 export default function Home() {
     const [adjacencyListStatus, setAdjacencyListStatus] = useState(false)
     const [adjacencyMatrixStatus, setAdjacencyMatrixStatus] = useState(false)
+    const [adjacencySetStatus, setAdjacencySetStatus] = useState(false)
     const [isLoading, setIsLoading] = useState(false)
     const [users, setUsers] = useState([])
     const [timeTakenAdjacencyList, setTimeTakenAdjacencyList] = useState(0)
     const [timeTakenAdjacencyMatrix, setTimeTakenAdjacencyMatrix] = useState(0)
+    const [timeTakenAdjacencySet, setTimeTakenAdjacencySet] = useState(0)
+
+    // Hardcode user as Gab
+    const LOGGED_USER = process.env.NODE_ENV === "development" ? "apple" : "dIIKEfOgo0KqUfGQvGikPg"
 
     useEffect(() => {
         getGraphStatus()
@@ -27,6 +33,8 @@ export default function Home() {
             const response = await getStatus()
             setAdjacencyListStatus(response.data.adjacencyListStatus)
             setAdjacencyMatrixStatus(response.data.adjacencyMatrixStatus)
+            //TODO
+            // setAdjacencySetStatus(response.data.adjacencySetStatus)
         } catch (error) {
             console.log(error)
         }
@@ -48,17 +56,19 @@ export default function Home() {
     const addFriend = async (toUser, graphType) => {
         try {
             // Check if environment is in testing or production mode and set username according to the dataset used
-            const fromUser = process.env.NODE_ENV === "development" ? "apple" : "dIIKEfOgo0KqUfGQvGikPg"
             setIsLoading(true)
-            const response = await addFriends(fromUser, toUser, graphType)
+            const response = await addFriends(LOGGED_USER, toUser, graphType)
             setUsers(response.data.friendSuggestions)
 
             if (graphType === "list") {
                 setAdjacencyListStatus(true)
                 setTimeTakenAdjacencyList(response.data.timeTaken)
-            } else {
+            } else if (graphType === "matrix") {
                 setAdjacencyMatrixStatus(true)
                 setTimeTakenAdjacencyMatrix(response.data.timeTaken)
+            } else {
+                setAdjacencySetStatus(true)
+                setTimeTakenAdjacencySet(response.data.timeTaken)
             }
 
             console.log(response)
@@ -81,6 +91,12 @@ export default function Home() {
         "bg-red-400": !adjacencyMatrixStatus,
     })
 
+    const adjacencySetStatusStyle = classnames({
+        "w-4/5 justify-self-center": true,
+        "bg-green-300": adjacencySetStatus,
+        "bg-red-400": !adjacencySetStatus,
+    })
+
     return (
         <div className="flex flex-col min-h-screen py-2 bg-black text-white">
             <Head>
@@ -88,7 +104,7 @@ export default function Home() {
                 <link rel="icon" href="/favicon.ico" />
             </Head>
             <div className="flex border divide-x h-96">
-                <div className="flex flex-col w-2/5 p-3 items-center space-y-3">
+                <div className="flex flex-col w-1/3 p-3 items-center space-y-3">
                     <BoxCard
                         title="Adjacency List"
                         setIsLoading={setIsLoading}
@@ -100,32 +116,7 @@ export default function Home() {
                     />
                 </div>
 
-                <div className="flex flex-col w-1/4 p-3 items-center">
-                    <span className="font-bold uppercase underline text-xl">Menu</span>
-                    <div className="w-full grid grid-cols-2 gap-y-3 mt-3 uppercase text-center font-semibold">
-                        <span className="pb-2 border-b">Status</span>
-                        <span className="pb-2 border-b">Indication</span>
-                        <span className="border-r">Adjacency List</span>
-                        <span className={adjacencyListStatusStyle} />
-                        <span className="border-r">Adjacency Matrix</span>
-                        <span className={adjacencyMatrixStatusStyle} />
-                    </div>
-                    <div className="flex w-full h-full items-center justify-center">
-                        {
-                            isLoading
-                                ? <PuffLoader color={"white"} size={175} margin={2} />
-                                : null
-                        }
-                    </div>
-                    <button
-                        className="w-full rounded-lg py-2 px-4 hover:bg-yellow-400 uppercase text-xl font-medium tracking-wide"
-                        onClick={getRandomTenFriends}
-                    >
-                        Find Random Friends
-                    </button>
-                </div>
-
-                <div className="flex flex-col w-2/5 p-3 items-center space-y-3">
+                <div className="flex flex-col w-1/3 p-3 items-center space-y-3">
                     <BoxCard
                         title="Adjacency Matrix"
                         setIsLoading={setIsLoading}
@@ -136,16 +127,54 @@ export default function Home() {
                         setTimeTaken={setTimeTakenAdjacencyMatrix}
                     />
                 </div>
+
+                <div className="flex flex-col w-1/3 p-3 items-center space-y-3">
+                    <BoxCard
+                        title="Adjacency Set"
+                        setIsLoading={setIsLoading}
+                        generate={generateAdjacencySet}
+                        remove={deleteAdjacencySet}
+                        setStatus={setAdjacencySetStatus}
+                        timeTaken={timeTakenAdjacencySet}
+                        setTimeTaken={setTimeTakenAdjacencySet}
+                    />
+                </div>
+
+                <div className="flex flex-col w-1/3 p-3 items-center">
+                    <span className="font-bold uppercase underline text-xl">Menu</span>
+                    <div className="w-full grid grid-cols-2 gap-y-3 mt-3 uppercase text-center font-semibold">
+                        <span className="pb-2 border-b">Status</span>
+                        <span className="pb-2 border-b">Indication</span>
+                        <span className="border-r min-w-min">Adjacency List</span>
+                        <span className={adjacencyListStatusStyle} />
+                        <span className="border-r min-w-min">Adjacency Matrix</span>
+                        <span className={adjacencyMatrixStatusStyle} />
+                        <span className="border-r min-w-min">Adjacency Set</span>
+                        <span className={adjacencySetStatusStyle} />
+                    </div>
+                    <div className="flex w-full h-full items-center justify-center">
+                        {
+                            isLoading
+                                ? <PuffLoader color={"white"} size={150} margin={2} />
+                                : null
+                        }
+                    </div>
+                    <button
+                        className="w-full rounded-lg py-2 px-4 hover:bg-yellow-400 uppercase text-xl font-medium tracking-wide"
+                        onClick={getRandomTenFriends}
+                    >
+                        Find Random Friends
+                    </button>
+                </div>
             </div>
             <div className="flex flex-wrap justify-around mt-2 h-full overflow-y-auto scrollbar-thin scrollbar-thumb-gray-400 scrollbar-track-white scrollbar-thumb-rounded-full scrollbar-track-rounded-full">
                 {
                     users && users.length !== 0
                         ? users.map(user => {
-                            const currentUser = process.env.NODE_ENV === "development" ? "apple" : "dIIKEfOgo0KqUfGQvGikPg"
                             // const userFriends = user.friends.split(",")
 
                             // Do not display current user
-                            if (!user || user.user_id === currentUser) {
+                            if (!user || user.user_id === LOGGED_USER) {
                                 return null
                             }
                             return <ProfileCard
@@ -157,8 +186,8 @@ export default function Home() {
                                 star={user.average_stars}
                                 useful={user.useful}
                                 joined={user.yelping_since}
-                                addFriendList={() => addFriend(user.user_id, "list")}         // Hardcore current user as Gabi
-                                addFriendMatrix={() => addFriend(user.user_id, "matrix")}     // Hardcore current user as Gabi
+                                addFriendList={() => addFriend(user.user_id, "list")}
+                                addFriendMatrix={() => addFriend(user.user_id, "matrix")}
                             />
                         })
                         : null
